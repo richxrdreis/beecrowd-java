c48b0ac6db419c459034b2330cecbdf64667ecb5
Richard Reis <richxrdreis@hotmail.com>
2025-08-20 15:33:52 -0300

    basic sum

--- src/E1001/E1001.java
+++ src/E1001/E1001.java
@@ -1,7 +1,18 @@
 package E1001;
 
+import java.util.Scanner;
+
 public class E1001 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        int soma = A + B;
+
+        System.out.println("X = " + soma);
+
+        sc.close();
     }
 }
