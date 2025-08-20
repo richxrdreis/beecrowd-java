b340cace1f64a0ab293e9b09f5e79acb94f1e6e9
Richard Reis <richxrdreis@hotmail.com>
2025-08-20 15:28:58 -0300

    Hello World!

---
+++ src/E1000/E1000.java
@@ -0,0 +1,7 @@
+package E1000;
+
+public class E1000 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/E1001/E1001.java
@@ -0,0 +1,7 @@
+package E1001;
+
+public class E1001 {
+    public static void main(String[] args) {
+        
+    }
+}
