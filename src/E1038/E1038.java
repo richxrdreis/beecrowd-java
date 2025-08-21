438f9fe16d16b75a60e126faa5e7ec496b2b6123
Richard Reis <richxrdreis@hotmail.com>
2025-08-21 14:12:38 -0300

    lanche

--- src/E1038/E1038.java
+++ src/E1038/E1038.java
@@ -1,4 +1,32 @@
 package E1038;
 
+import java.util.Scanner;
+
 public class E1038 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int codigo = sc.nextInt();
+        int quantidade = sc.nextInt();
+
+        double preco = 0.0;
+
+        if (codigo == 1) {
+            preco = 4.00;
+        } else if (codigo == 2) {
+            preco = 4.50;
+        } else if (codigo == 3) {
+            preco = 5.00;
+        } else if (codigo == 4) {
+            preco = 2.00;
+        } else if (codigo == 5) {
+            preco = 1.50;
+        }
+
+        double total = preco * quantidade;
+
+        System.out.printf("Total: R$ %.2f%n", total);
+
+        sc.close();
+    }
 }
