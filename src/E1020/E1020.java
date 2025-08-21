ca00b0e3f13a5dc9fd79b67f5c66a893e222d644
Richard Reis <richxrdreis@hotmail.com>
2025-08-21 14:10:10 -0300

    idade em dias

--- src/E1020/E1020.java
+++ src/E1020/E1020.java
@@ -1,4 +1,21 @@
 package E1020;
 
+import java.util.Scanner;
+
 public class E1020 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int totalDias = sc.nextInt();
+
+        int anos = totalDias / 365;
+        int meses = (totalDias % 365) / 30;
+        int dias = (totalDias % 365) % 30;
+
+        System.out.println(anos + " ano(s)");
+        System.out.println(meses + " mes(es)");
+        System.out.println(dias + " dia(s)");
+
+        sc.close();
+    }
 }
