5e20e0f34f90c21e802f5616c90c2e1cf6f35009
Richard Reis <richxrdreis@hotmail.com>
2025-08-21 14:18:48 -0300

    aumento de salario

--- src/E1048/E1048.java
+++ src/E1048/E1048.java
@@ -1,4 +1,33 @@
 package E1048;
 
+import java.util.Scanner;
+
 public class E1048 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double salario = sc.nextDouble();
+        double percentual;
+
+        if (salario <= 400.00) {
+            percentual = 15;
+        } else if (salario <= 800.00) {
+            percentual = 12;
+        } else if (salario <= 1200.00) {
+            percentual = 10;
+        } else if (salario <= 2000.00) {
+            percentual = 7;
+        } else {
+            percentual = 4;
+        }
+
+        double reajuste = salario * percentual / 100.0;
+        double novoSalario = salario + reajuste;
+
+        System.out.printf("Novo salario: %.2f%n", novoSalario);
+        System.out.printf("Reajuste ganho: %.2f%n", reajuste);
+        System.out.printf("Em percentual: %.0f %%%n", percentual);
+
+        sc.close();
+    }
 }
