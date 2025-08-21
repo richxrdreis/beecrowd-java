7f271a4a4318d0e95dd6e8c42cd6ba46169057f5
Richard Reis <richxrdreis@hotmail.com>
2025-08-21 14:07:28 -0300

    sphere

--- src/E1011/E1011.java
+++ src/E1011/E1011.java
@@ -1,4 +1,20 @@
 package E1011;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class E1011 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double R = sc.nextDouble();
+        double pi = 3.14159;
+
+        double volume = (4.0 / 3) * pi * Math.pow(R, 3);
+
+        System.out.printf("VOLUME = %.3f%n", volume);
+
+        sc.close();
+    }
 }
