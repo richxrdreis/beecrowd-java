df155b33de9d6661ebd3f49472befb7c84263824
Richard Reis <richxrdreis@hotmail.com>
2025-08-21 14:13:12 -0300

    simple sort

--- src/E1042/E1042.java
+++ src/E1042/E1042.java
@@ -1,4 +1,32 @@
 package E1042;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class E1042 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] original = new int[3];
+        original[0] = sc.nextInt();
+        original[1] = sc.nextInt();
+        original[2] = sc.nextInt();
+
+        int[] crescente = original.clone();
+        Arrays.sort(crescente); // ordena em ordem crescente
+
+        // imprime em ordem crescente
+        for (int i = 0; i < 3; i++) {
+            System.out.println(crescente[i]);
+        }
+
+        System.out.println(); // linha em branco
+
+        // imprime na ordem original
+        for (int i = 0; i < 3; i++) {
+            System.out.println(original[i]);
+        }
+
+        sc.close();
+    }
 }
