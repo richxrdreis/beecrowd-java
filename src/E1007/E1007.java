0e5005588156a5164de012a31bc70f1593ca9450
Richard Reis <richxrdreis@hotmail.com>
2025-08-21 14:06:27 -0300

    diferenca

--- src/E1007/E1007.java
+++ src/E1007/E1007.java
@@ -1,4 +1,20 @@
 package E1007;
 
+import java.util.Scanner;
+
 public class E1007 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int D = sc.nextInt();
+
+        int DIFERENCA = (A * B - C * D);
+
+        System.out.println("DIFERENCA = " + DIFERENCA);
+
+        sc.close();
+    }
 }
