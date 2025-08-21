393ecdb84c88fe6a6e5e4dc9f812852dcbc1bcf3
Richard Reis <richxrdreis@hotmail.com>
2025-08-21 14:06:40 -0300

    salary

--- src/E1008/E1008.java
+++ src/E1008/E1008.java
@@ -1,4 +1,22 @@
 package E1008;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class E1008 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int numero = sc.nextInt();
+        int horas = sc.nextInt();
+        double valorHora = sc.nextDouble();
+
+        double salario = horas * valorHora;
+
+        System.out.printf("NUMBER = %d%n", numero);
+        System.out.printf("SALARY = U$ %.2f%n", salario);
+
+        sc.close();
+    }
 }
