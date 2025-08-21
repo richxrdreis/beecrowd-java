8c477903e492c91fb2ae7cb9c615f8bd5b125540
Richard Reis <richxrdreis@hotmail.com>
2025-08-21 14:17:41 -0300

    tempo de jogo

--- src/E1046/E1046.java
+++ src/E1046/E1046.java
@@ -1,4 +1,24 @@
 package E1046;
 
+import java.util.Scanner;
+
 public class E1046 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int inicio = sc.nextInt();
+        int fim = sc.nextInt();
+
+        int duracao;
+
+        if (inicio < fim) {
+            duracao = fim - inicio;
+        } else {
+            duracao = 24 - inicio + fim;
+        }
+
+        System.out.printf("O JOGO DUROU %d HORA(S)%n", duracao);
+
+        sc.close();
+    }
 }
