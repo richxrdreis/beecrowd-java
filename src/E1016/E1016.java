418dd40544c778523a29d28c6bd0232e1fdcbe2e
Richard Reis <richxrdreis@hotmail.com>
2025-08-21 14:08:41 -0300

    distance

--- src/E1016/E1016.java
+++ src/E1016/E1016.java
@@ -1,4 +1,18 @@
 package E1016;
 
+import java.util.Scanner;
+
 public class E1016 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int distancia = sc.nextInt();
+
+        // O carro Y se aproxima 1 km a cada 2 minutos
+        int tempo = distancia * 2;
+
+        System.out.println(tempo + " minutos");
+
+        sc.close();
+    }
 }
