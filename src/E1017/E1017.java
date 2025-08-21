9d06f39e0c9a752223c5b1bc32084cbc0289d0f9
Richard Reis <richxrdreis@hotmail.com>
2025-08-21 14:09:01 -0300

    fuel consumption

--- src/E1017/E1017.java
+++ src/E1017/E1017.java
@@ -1,4 +1,24 @@
 package E1017;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class E1017 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int tempo = sc.nextInt();
+        int velocidade = sc.nextInt();
+
+        // Distância = tempo * velocidade
+        double distancia = tempo * velocidade;
+
+        // Consumo = distância / 12 (KM/L)
+        double litros = distancia / 12.0;
+
+        System.out.printf("%.3f%n", litros);
+
+        sc.close();
+    }
 }
