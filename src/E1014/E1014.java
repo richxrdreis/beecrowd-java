fa054b902c6c47729dc3357b76c1ed6396cab2a3
Richard Reis <richxrdreis@hotmail.com>
2025-08-21 14:07:54 -0300

    consumo

--- src/E1014/E1014.java
+++ src/E1014/E1014.java
@@ -1,4 +1,20 @@
 package E1014;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class E1014 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int X = sc.nextInt();
+        double Y = sc.nextDouble();
+
+        double consumo = X / Y;
+
+        System.out.printf("%.3f km/l%n", consumo);
+
+        sc.close();
+    }
 }
