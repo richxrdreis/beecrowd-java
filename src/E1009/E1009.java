85207b57f90a7192df8969ecfe9bf1bc77576485
Richard Reis <richxrdreis@hotmail.com>
2025-08-21 14:06:55 -0300

    salary with bonus

--- src/E1009/E1009.java
+++ src/E1009/E1009.java
@@ -1,4 +1,21 @@
 package E1009;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class E1009 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        String nome = sc.next();
+        double salarioFixo = sc.nextDouble();
+        double vendas = sc.nextDouble();
+
+        double totalReceber = salarioFixo + (vendas * 0.15);
+
+        System.out.printf("TOTAL = R$ %.2f%n", totalReceber);
+
+        sc.close();
+    }
 }
